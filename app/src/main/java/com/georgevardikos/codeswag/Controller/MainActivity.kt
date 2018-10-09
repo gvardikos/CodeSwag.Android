fd76530da53f011dbdcc370314ba381a3bb14b4a
gvardikos <giorgosvardikos@gmail.com>
2018-10-09 17:56:03 +0300

    lesson02

--- app/src/main/java/com/georgevardikos/codeswag/Controller/MainActivity.kt
+++ app/src/main/java/com/georgevardikos/codeswag/Controller/MainActivity.kt
@@ -2,13 +2,21 @@ package com.georgevardikos.codeswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.georgevardikos.codeswag.Model.Category
 import com.georgevardikos.codeswag.R
+import com.georgevardikos.codeswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        main_categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/georgevardikos/codeswag/Model/Category.kt
+++ app/src/main/java/com/georgevardikos/codeswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.georgevardikos.codeswag.Model
 /**
  * Created by gvardikos on 09/10/2018.
  */
-class Category(title: String, image: String)
+class Category(var title: String, var image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
